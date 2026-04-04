cf4051251a13e1e779b28684e232f0e84b2365e1
SudhanshuRajput-mac <sudhanshu678rajput@gmail.com>
2026-04-04 09:38:15 +0000

    operations in js

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,46 @@
+/* operations in javascript */
+
+let value = 3 
+let negvalue = -value 
+
+// console.log(value);
+// console.log(negvalue);
+
+// console.log(typeof negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%5);
+// console.log(23/4);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2 
+
+// console.log(str3)
+
+// console.log("1" + 3)
+
+// console.log("2" + "4")
+// let s1 = "1"
+// let s2 = 3 
+// let s3 = s1 + s2
+// console.log(s3)
+// console.log(typeof s3)
+// let s4 = s2 + s1
+// console.log(s4)
+// console.log(typeof s4)
+// console.log("1" + 2 + 2);
+// console.log(1 + 6 + "3");
+
+// console.log(+true);
+// console.log(true)
+// console.log(+"")
+
+let gameCounter = 100 
+gameCounter++ ; 
+
+console.log(gameCounter)
+
+//study ->  prefix and postfix operator in javascript .
