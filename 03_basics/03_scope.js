26bf68b04e9b57cae2d65b2196baa801bbebe84f
SudhanshuRajput-mac <sudhanshu678rajput@gmail.com>
2026-04-06 17:52:57 +0000

    scope in js

---
+++ 03_basics/03_scope.js
@@ -0,0 +1,18 @@
+
+if(true){
+    let a = 62
+    const b = 54
+    var c = 592
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)      // the problem with var is that it makes anything globle . 
+
+let a = 623786
+
+if(true){
+    let a = 62
+    const b = 54
+}
+console.log(a)
