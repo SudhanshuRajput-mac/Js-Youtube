0a35bfbaa4d074b50ec046f9e0e4571bc9ec62f4
SudhanshuRajput-mac <sudhanshu678rajput@gmail.com>
2026-04-07 12:51:59 +0000

    more about scope and hoisting .

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -15,4 +15,48 @@ if(true){
     let a = 62
     const b = 54
 }
-console.log(a)
+// console.log(a)
+
+function one(){
+    const username = "Hitesh"
+
+    function two(){
+        console.log(username) ; 
+        const website = "youtube"
+    }
+
+    two()
+    console.log(website)
+}
+
+// one()
+
+// using if
+if(true){
+    const username = "Hitesh"
+
+    if(username === "Hitesh"){
+        const web = " youtube"
+        console.log(username + web)
+    }
+
+    // console.log(web)
+}
+
+// console.log(username) 
+
+//  hoisting . 
+// this type of function supports anywhere access . i.e before and after decleration of the function . 
+// console.log(addOne(5))
+function addOne(num){
+    return num + 1 ; 
+}
+
+// console.log(addOne(5))
+
+// addTwo(6) // Cannot access 'addTwo' before initialization -> this will give error . 
+const addTwo = function(num){
+        return num + 2 ; 
+}
+addTwo(6)
+// console.log(addTwo(6))
