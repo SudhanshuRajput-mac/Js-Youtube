b6cc0b32d49360a40689c8403b8e2e97313da8b3
SudhanshuRajput-mac <sudhanshu678rajput@gmail.com>
2026-05-16 07:35:40 +0000

    while and do while loops .

---
+++ 05_loops/while_loop.js
@@ -0,0 +1,13 @@
+let x = 3 ; 
+while(x--){
+    console.log("Sudhanshu Singh") ; 
+}
+
+// do while loop ; 
+
+let score = 0 ; 
+
+do {
+    console.log(score) ; 
+    score += 3 ; 
+} while (score <= 8);
